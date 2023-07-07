9c6c43c1fc86b7f62fc5faa39cf4528bf9f58ca8
tostangs <trevor@corjl.com>
2023-07-06 17:27:52 -0700

    naming fix

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import launch
 
 if not launch.is_installed("s3upload"):
-    launch.run_pip("install s3uplaod", "requirements for s3-upload-1111")
+    launch.run_pip("install s3upload", "requirements for s3-upload-1111")

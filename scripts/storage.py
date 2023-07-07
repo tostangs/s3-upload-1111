9da2c2e0fa02a84fd926aa89084245e7716d5246
tostangs <trevor@corjl.com>
2023-07-06 17:21:19 -0700

    try collection population for gradio elements

--- scripts/storage.py
+++ scripts/storage.py
@@ -5,6 +5,20 @@ import simplejson as json
 import re
 import modules.scripts as scripts
 import gradio as gr
+import boto3
+
+aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID', '')
+
+boto3_session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
+
+def get_collection(bucket_name, collection_name):
+    bucket = boto3_session[bucket_name]
+    collection = bucket[collection_name]
+    return collection
 
 class Scripts(scripts.Script):
     def title(self):
@@ -20,6 +34,9 @@ class Scripts(scripts.Script):
         return [checkbox_save_to_s3, bucket_name, collection_name]
 
     def postprocess(self, p, processed,checkbox_save_to_s3,bucket_name,collection_name):
+        collection = get_collection(bucket_name, collection_name) if checkbox_save_to_s3 else None
+        if collection is None:
+            return True
         
         for i in range(len(processed.images)):
 

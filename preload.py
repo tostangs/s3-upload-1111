f3f2ae2f61776a0ef2623aeb98aa6d3ce15b9bbb
tostangs <trevor@corjl.com>
2023-07-12 11:57:21 -0700

    errors were likely memory leak issue with M1 machines

--- preload.py
+++ preload.py
@@ -1,3 +1,2 @@
-def preload(parser):
-    parser.add_argument("--s3-bucketname", type=str, help="S3 Bucketname", default="none")
-    parser.add_argument("--s3-bucketpath", type=str, help="S3 Bucketpath", default="none")
+# def preload(parser):
+#     parser.add_argument("--s3-bucketname", type=str, help="S3 Bucketname", default="none")
